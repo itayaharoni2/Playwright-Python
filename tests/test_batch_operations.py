325566a2a096c166e14cf4f888ddec015d83f760
Itay Aharoni <itay1itay@gmail.com>
2025-04-12 18:06:48 +0300

    Minor changes in some files

--- tests/test_batch_operations.py
+++ tests/test_batch_operations.py
@@ -1,12 +1,11 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 from pages.todos_page import TodosPage
-# what to do with the asserts?
 
 def test_mark_all_items_as_completed(page: Page, base_url: str):
     todos = TodosPage(page)
     todos.navigate(base_url)
 
-    todos.add_todo("Do the Dished") 
+    todos.add_todo("Do the Dishes") 
     todos.add_todo("Mop the floor")
     todos.add_todo("Cook Pasta")
 
@@ -14,7 +13,7 @@ def test_mark_all_items_as_completed(page: Page, base_url: str):
 
     for i in range(3): 
         item = todos.todo_items.nth(i)
-        assert "completed" in item.get_attribute("class")
+        expect(item).to_have_class(lambda value: "completed" in value) # Check if every item has the completed class
 
 def test_clear_completed_removes_completed_items(page: Page, base_url: str):
     todos = TodosPage(page)
@@ -22,11 +21,11 @@ def test_clear_completed_removes_completed_items(page: Page, base_url: str):
 
     todos.add_todo("Wash the Dishes")
     todos.add_todo("Make cocktails")
-    todos.mark_todo_completed(0) # Marks the first one
+    todos.mark_todo_completed(0)
 
     todos.clear_completed()
 
     texts = todos.get_todo_texts()
-    assert "Wash the Dishes" not in texts # The first one should be removed
-    assert "Make cocktails" in texts 
-    assert len(texts) == 1 # Only one item should be left
+    expect(texts).not_to_contain("Wash the Dishes") # Check if the completed item is removed
+    expect(texts).to_contain("Make cocktails") # Check if the incomplete item is still there
+    expect(len(texts)).to_equal(1) # Only one item should remain
--- tests/test_visual_testing.py
+++ tests/test_visual_testing.py
@@ -1,7 +1,6 @@
 from playwright.sync_api import Page
-from pages.todos_page import TodosPage
 
-# Bonus - visual testing
+# visual testing
 def test_visual_snapshot(page: Page, base_url: str):
     page.goto(base_url)
     page.screenshot(path="snapshots/homepage.png", full_page=True)
